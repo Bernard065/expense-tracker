ead7969cdd2148072d85e49d2462b0858fafde94
Bernard <bebenibernard@gmail.com>
2026-03-04 17:06:41 +0300

    Add MongoDB database connection configuration and integrate with server

---
+++ expense-backend/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  console.log('Attempting to connect to MongoDB...');
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI!);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${(error as Error).message}`);
+  }
+};
